1d9c9a07d7f6a14ff1489345eeea0db940abfabd
Jose Arias <josea4706@gmil.com>
2023-10-31 20:26:57 -0400

    HOMEWORKS RESUELTOS 31-10-2023

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,7 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,26 +12,35 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   return x + y
 }
+suma (3, 3)
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   return x - y
 }
+resta (10, 20);
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   return x / y
 }
+divide (20, 2);
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   return x * y
 }
+multiplica (5, 5);
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   return (x % y)
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
